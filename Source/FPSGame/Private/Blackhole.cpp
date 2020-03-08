8911e3c95190a3cbea19b753594599babd8141ca
chrisdupuy-dev <christopherdupuy@mindfulabstraction.com>
2020-03-08 19:35:29 +1100

    Adding AI Guard

--- Source/FPSGame/Private/Blackhole.cpp
+++ Source/FPSGame/Private/Blackhole.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "Blackhole.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/SphereComponent.h"
-#include "Blackhole.h"
 
 #include "Kismet/GameplayStatics.h"
 
--- Source/FPSGame/Private/FPSObjectiveActor.cpp
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/SphereComponent.h"
 #include "FPSObjectiveActor.h"
+
+#include "Components/SphereComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "FPSCharacter.h"
 #include "Kismet/GameplayStatics.h"
